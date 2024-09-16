b65db1028ee151c7da56b7c707d87e361f8918c1
Nazım Sucu <99741791+nazms23@users.noreply.github.com>
2024-09-16 17:12:13 +0300

    bilgilendirme

--- BankaSifreUyg/src/ayarsayfa/Bilgilendirme.js
+++ BankaSifreUyg/src/ayarsayfa/Bilgilendirme.js
@@ -1,10 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Linking, Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 const Bilgilendirme = () => {
+  
+  //KAAN BUNU ÜŞENDİM IGSDUYFGSUYFGDSFHUYDF sen yap 0 dan tasarımını
   return (
     <View>
-      <Text>Bilgilendirme</Text>
+      <Text>Kullanımınız açısından reklamların kötü bir deneyim yaşatabilceğini düşündüğümüzden reklam koymadık. Sırf şifrelerinize bakmak için girdiğiniz bi uygulamadan zırt pırt reklam çıksa not defteri yerine niye bunu tercih edesiniz. Bu yüzden de bağış falan yapmak isterseniz insta: Kaan1iq0 bana yazın iban atam</Text>
+      <Text>Tamamen açık kaynak kodlu alın bu github burdan bakın kodlarına. Eklemek istediğiniz bişi varsada ekleyin</Text>
+      <Pressable style={{backgroundColor:'gray', width:200}}  onPress={async()=>{
+        await Linking.openURL('https://github.com/nazms23/RN-BankaSifreUyg')
+      }}>
+        <Text style={{color:'lime'}}>BURAYA GİTHUB LOGO KONULCAK BANA TIKLA</Text>
+      </Pressable>
+      <View>
+        <Text>Katkıda bulunanlar</Text>
+        <Text>KAAN1İQ0</Text>
+        <Text>NAZIM</Text>
+        <Text>Gelecekte uygulamayı geliştirmeye yardım edicek olan o yakışıklı/güzel insan</Text>
+      </View>
     </View>
   )
 }
